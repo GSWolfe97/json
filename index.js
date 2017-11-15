0fe71d9637342cd6b63598422ae2cc901ba7180e
Garrett <gw0815858@otc.edu>
2017-11-14 18:09:06 -0600

    Files

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-var _ = require('lodash');
-console.log('I kept getting errors and could not understand how to do this.');
+var express = require('express');
+var app = express();
+var path = require('path');
+ 
+app.get('/', function (req, res) {
+  //res.send('Hello World');
+  res.sendFile(path.join(__dirname + "/index.html"));
+})
+app.get('/aboutus.html', function (req, res) {
+    res.sendFile(path.join(__dirname + "/aboutus.html"));
+})
+ 
+app.listen(3000);
